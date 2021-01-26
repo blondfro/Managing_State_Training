d70fea0dd14692e58a344d15272f671947b25124
James Morris <blondfro@gmail.com>
2021-01-26 15:44:53 -0600

    created error checking for form submission.  Currently displays at the top of the form after submission attempt.

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -20,6 +20,10 @@ export default function Checkout({ cart, emptyCart }) {
   const [status, setStatus] = useState(STATUS.IDLE);
   const [saveError, setSaveError] = useState(null);
 
+  // Derived state
+  const errors = getErrors(address);
+  const isValid = Object.keys(errors).length === 0;
+
   function handleChange(e) {
     e.persist(); //persist the event for react
     setAddress((curAddr) => {
@@ -34,13 +38,24 @@ export default function Checkout({ cart, emptyCart }) {
   async function handleSubmit(event) {
     event.preventDefault();
     setStatus(STATUS.SUBMITTING);
-    try {
-      await saveShippingAddress(address);
-      emptyCart();
-      setStatus(STATUS.COMPLETED);
-    } catch (e) {
-      setSaveError(e);
+    if (isValid) {
+      try {
+        await saveShippingAddress(address);
+        emptyCart();
+        setStatus(STATUS.COMPLETED);
+      } catch (e) {
+        setSaveError(e);
+      }
+    } else {
+      setStatus(STATUS.SUBMITTED);
     }
+  }
+
+  function getErrors(address) {
+    const results = {};
+    if (!address.city) results.city = "City is required";
+    if (!address.country) results.country = "Country is required";
+    return results;
   }
 
   if (saveError) throw saveError;
@@ -51,6 +66,16 @@ export default function Checkout({ cart, emptyCart }) {
   return (
     <>
       <h1>Shipping Info</h1>
+      {!isValid && status === STATUS.SUBMITTED && (
+        <div role="alert">
+          <p>Please fix the following errors:</p>
+          <ul>
+            {Object.keys(errors).map((key) => {
+              return <li key={key}>{errors[key]}</li>;
+            })}
+          </ul>
+        </div>
+      )}
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="city">City</label>
