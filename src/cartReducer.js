1c4101c4867b0a6bbda193f214e61ccc6eab3824
James Morris <blondfro@gmail.com>
2021-01-27 16:16:16 -0600

    updated the cart reducer to have the code that is currently in the app component.

--- src/cartReducer.js
+++ src/cartReducer.js
@@ -1,9 +1,25 @@
 export default function cartReducer(cart, action) {
   switch (action.type) {
-    case "add":
-      return [];
-    case "update":
-      return [];
+    case "add": {
+      const { id, sku } = action;
+      const itemInCart = cart.find((item) => item.sku === sku);
+      if (itemInCart) {
+        // Return new array with the correct item replaced.
+        return cart.map((item) =>
+          item.sku === sku ? { ...item, quantity: item.quantity + 1 } : item
+        );
+      } else {
+        // Return new array with the new item added in.
+        return [...cart, { id, sku, quantity: 1 }];
+      }
+    }
+    case "updateQuantity": {
+      // check if the quantity is 0, if yes, remove item, if not return new array.
+      const { sku, quantity } = action;
+      return quantity === 0
+        ? cart.filter((item) => item.sku !== sku)
+        : cart.map((item) => (item.sku === sku ? { ...item, quantity } : item));
+    }
     case "empty":
       return [];
     default: {
