d2248b39184a32a4f8d674295343d14374f9ba7d
James Morris <blondfro@gmail.com>
2021-01-28 12:30:52 -0600

    removed the cart state functions from the app to the cart reducer and modified the remaining components to use dispatch instead of the original function calls.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer } from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Footer from "./Footer";
@@ -7,51 +7,22 @@ import Products from "./Products";
 import Cart from "./Cart";
 import Detail from "./Detail";
 import Checkout from "./Checkout";
+import cartReducer from "./cartReducer";
+
+let initialCart;
+try {
+  initialCart = JSON.parse(localStorage.getItem("cart")) ?? [];
+} catch {
+  console.error("The cart could not be parsed into JSON.");
+  initialCart = [];
+}
 
 export default function App() {
-  const [cart, setCart] = useState(() => {
-    try {
-      return JSON.parse(localStorage.getItem("cart")) ?? [];
-    } catch {
-      console.error("The cart could not be parsed into JSON.");
-      return [];
-    }
-  });
+  const [cart, dispatch] = useReducer(cartReducer, initialCart);
 
   useEffect(() => {
     localStorage.setItem("cart", JSON.stringify(cart));
   }, [cart]);
-
-  function addToCart(id, sku) {
-    setCart((items) => {
-      const itemInCart = items.find((item) => item.sku === sku);
-      if (itemInCart) {
-        // Return new array with the correct item replaced.
-        return items.map((item) =>
-          item.sku === sku ? { ...item, quantity: item.quantity + 1 } : item
-        );
-      } else {
-        // Return new array with the new item added in.
-        return [...items, { id, sku, quantity: 1 }];
-      }
-    });
-  }
-
-  function updateQuantity(sku, quantity) {
-    setCart((items) => {
-      // check if the quantity is 0, if yes, remove item, if not return new array.
-
-      return quantity === 0
-        ? items.filter((item) => item.sku !== sku)
-        : items.map((item) =>
-            item.sku === sku ? { ...item, quantity } : item
-          );
-    });
-  }
-
-  function emptyCart() {
-    setCart([]);
-  }
 
   return (
     <>
@@ -66,15 +37,15 @@ export default function App() {
             <Route path="/:category" element={<Products />} />
             <Route
               path="/:category/:id"
-              element={<Detail addToCart={addToCart} />}
+              element={<Detail dispatch={dispatch()} />}
             />
             <Route
               path="/cart"
-              element={<Cart cart={cart} updateQuantity={updateQuantity} />}
+              element={<Cart cart={cart} dispatch={dispatch()} />}
             />
             <Route
               path="/checkout"
-              element={<Checkout cart={cart} emptyCart={emptyCart} />}
+              element={<Checkout cart={cart} dispatch={dispatch()} />}
             />
           </Routes>
         </main>
